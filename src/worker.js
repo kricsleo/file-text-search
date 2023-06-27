082596b23cd08be2ec5f10166da3287224bdf500
许圣峰 <kricsleo@163.com>
2023-06-27 19:16:49 +0800

    feat: search file in worker

---
+++ src/worker.js
@@ -0,0 +1,27 @@
+import fs from 'fs/promises'
+// import { Search, Options } from './types'
+
+export function searchContent(content, search) {
+  const searchReg = typeof search === 'string' 
+    ? new RegExp(search, 'g') 
+    : new RegExp(search.source, search.global ? search.flags : search.flags + 'g')
+  const matches = []
+  for (const match of content.matchAll(searchReg)) {
+    matches.push({ 
+      match: match[0], 
+      start: match.index, 
+      end: match.index + match[0].length
+    })
+  }
+  return matches
+}
+
+export async function searchFile(options) {
+  const content = await fs.readFile(options.file, 'utf-8')
+  const allMatches = options.search.map(search => {
+    const matches = searchContent(content, search)
+    return { search, matches }
+  })
+  const matches = allMatches.filter(match => match.matches.length)
+  return matches
+}
